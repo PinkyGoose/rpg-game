e358bb40279fd76059816d11aee0312856a5e2ca
Vladimir <lachuginv@list.ru>
2024-08-10 01:18:33 +0300

    Крутой переход по уровням

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,14 @@
 //! Renders a 2D scene containing a single, moving sprite.
 
-use crate::spawn::{cache_entry_point_locations, EntryPoint, EntryPointBundle, LevelEntryPoints, SpawnPointBundle};
+use crate::player::spawn_player;
+use crate::spawn::{ SpawnPointId, UnresolvedIdRef};
+use crate::spawn::{cache_entry_point_locations, EntryPoint, EntryPointBundle, LevelEntryPoints,  SpawnPointBundle};
 use crate::spawn::SpawnPoint;
 use crate::goat::GoatBundle;
 use crate::player::{Player, PlayerBundle};
 use crate::constants::GRID_SIZE;
 use bevy_ecs_ldtk::LdtkSpriteSheetBundle;
-use bevy::prelude::{KeyCode, ResMut};
+use bevy::prelude::{Entity, KeyCode, ResMut};
 use bevy::prelude::ButtonInput;
 use bevy::prelude::With;
 use bevy_ecs_ldtk::LdtkWorldBundle;
@@ -36,6 +38,7 @@ use bevy::prelude::PluginGroup;
 use bevy_ecs_ldtk::app::LdtkEntityAppExt;
 use bevy_ecs_ldtk::app::LdtkIntCellAppExt;
 use bevy::prelude::IVec2;
+use bevy_inspector_egui::quick::WorldInspectorPlugin;
 use crate::movement::{move_all, move_player_from_input, randomize_movements};
 
 mod wall;
@@ -49,16 +52,20 @@ fn main() {
     App::new()
         .add_plugins(DefaultPlugins.set(ImagePlugin::default_nearest()))
         .add_plugins(bevy_ecs_ldtk::LdtkPlugin)
+        .add_plugins(WorldInspectorPlugin::new())
         // .add_plugins(FrameTimeDiagnosticsPlugin::default())
         .add_systems(Startup, setup)
-        .insert_resource(LevelSelection::index(0))
+        .insert_resource(LevelSelection::iid("bbd618c0-4ce0-11ef-9196-9768dcadd1bb"))
         .register_ldtk_entity::<PlayerBundle>("Player")
         .register_ldtk_entity::<GoatBundle>("Goat")
         .register_ldtk_entity::<SpawnPointBundle>("SpawnPoint")
         .register_ldtk_entity::<EntryPointBundle>("EntryPoint")
         .register_ldtk_int_cell::<WallBundle>(1)
         .init_resource::<LevelWalls>()
+        .register_type::<SpawnPointId>()
+        .register_type::<UnresolvedIdRef>()
         .init_resource::<LevelEntryPoints>()
+        .insert_resource(SpawnPointId(None))
         .add_systems(
             Update,
             (
@@ -68,7 +75,8 @@ fn main() {
                 cache_entry_point_locations,
                 move_all,
                 randomize_movements,
-                check_goal
+                check_goal,
+                spawn_player
             )
         )
         .run();
@@ -97,22 +105,36 @@ fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
 }
 
 fn check_goal(
-    level_selection: ResMut<LevelSelection>,
-    players: Query<&Transform, (With<Player>, Changed<Transform>)>,
-    goals: Query<&GridCoords, With<EntryPoint>>,
+    mut spawn_point_id: ResMut<SpawnPointId>,
+    mut level_selection: ResMut<LevelSelection>,
+    // mut respawn_point: ResMut<SpawnPoint>,
+    players: Query<(Entity,&Transform), (With<Player>, Changed<Transform>)>,
+    entries: Query<&GridCoords, With<EntryPoint>>,
     level_entries: Res<LevelEntryPoints>,
+
 ) {
-    if players
-        .iter()
-        .zip(goals.iter())
-        .any(|(player_grid_coords,_)|level_entries.in_entry_point_with_size(&player_grid_coords.translation.truncate(), 16) )
+    // let k = players
+    //     .iter();
+    // let j = k.zip(goals.iter());
+    let mut dest = None;
+    for (id, player_grid_coords) in players.iter(){
+            for entry in entries.iter(){
+                if let Some(level) = level_entries.in_entry_point_with_size(&player_grid_coords.translation.truncate(), 16){
+                    dest = Some(level);
+                    spawn_point_id.0 = Some(level.spawn_point.clone());
+                }
+            }
+        }
+    if let Some(dest) = dest
     {
         info!("Пришли");
-        let indices = match level_selection.into_inner() {
-            LevelSelection::Indices(indices) => indices,
-            _ => panic!("level selection should always be Indices in this game"),
-        };
+        // let mut iid = match level_selection.into_inner() {
+        //     LevelSelection::Iid(iid) => iid,
+        //     _ => panic!("level selection should always be iid in this game"),
+        // };
 
-        indices.level += 1;
+        *level_selection = LevelSelection::iid(dest.level.clone())
+        // let mut level = dest.level.clone();
+        // iid = &mut level;
     }
 }
--- src/player.rs
+++ src/player.rs
@@ -1,8 +1,10 @@
 use bevy::input::ButtonInput;
-use bevy::prelude::{Bundle, Component, KeyCode, Query, Res, Transform, With};
-use bevy_ecs_ldtk::{GridCoords, LdtkSpriteSheetBundle};
+use bevy::prelude::{Added, Bundle, Commands, Component, Entity, EventReader, KeyCode, Query, Res, ResMut, Transform, With, Without};
+use bevy_asset::AssetServer;
+use bevy_ecs_ldtk::{EntityIid, EntityInstance, GridCoords, LdtkSpriteSheetBundle, LevelEvent};
 use bevy_ecs_ldtk_macros::LdtkEntity;
 use crate::movement::{Character, MovementSpeed};
+use crate::spawn::{EntryPoint, SpawnPoint, SpawnPointId, UnresolvedIdRef};
 use crate::wall::LevelWalls;
 
 
@@ -17,5 +19,32 @@ pub struct PlayerBundle {
     // grid_coords: GridCoords,
     character: Character,
     movement_speed: MovementSpeed,
+
     // transform: Transform,
+}
+
+
+pub fn spawn_player(
+    mut level_events: EventReader<LevelEvent>,
+    mut player: Query<&mut Transform, With<Player>>,
+    spawn_points: Query<(&EntityIid, &Transform), (With<SpawnPoint>, Without<Player>)>,
+    mut spawn_point: ResMut<SpawnPointId>
+)
+{
+    for level_event in level_events.read() {
+        if let LevelEvent::Spawned(_) = level_event {
+                if let Some(point) = spawn_point.0.clone(){
+                    for (spawn, transform) in spawn_points.iter(){
+                        if *spawn==point{
+
+                            if let Ok(mut player_coords) = player.get_single_mut(){
+                                player_coords.translation = transform.translation;
+                            }
+                        }
+                    }
+                    spawn_point.0 = None;
+                    return;
+                }
+        }}
+
 }
